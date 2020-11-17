4ae48ae477654c4e66f879be950692a8377b9075
sf-12 <s.fukasawa12@gmail.com>
2020-11-17 09:54:57 +0000

    add some pages

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,4 +1,12 @@
 class TodolistsController < ApplicationController
+  def index 
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
   def new
     @list = List.new
   end
@@ -8,9 +16,11 @@ class TodolistsController < ApplicationController
     list = List.new(list_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # ３. トップ画面へリダイレクト
-    redirect_to '/top'
+    # ３. 詳細画面へリダイレクト
+    redirect_to todolist_path(list.id)
   end
+
+
 
   private
   # ストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'todolists/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
+  get  'top' => 'homes#top'
+  get  'todolists' => 'todolists#index'
+  get  'todolist/:id' => 'todolists#show', as: 'todolist'
+  get  'todolists/new' => 'todolists#new'
   post 'todolists' => 'todolists#create'
+
 end
